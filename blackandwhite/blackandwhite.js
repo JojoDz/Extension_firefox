9d45030ddf166bf7ce05ee78a24d10d804eb7c99
JojoDz <72552227+JojoDz@users.noreply.github.com>
2020-10-26 17:22:11 +0100

    ajout filtres

--- blackandwhite/blackandwhite.js
+++ blackandwhite/blackandwhite.js
@@ -1,3 +1,9 @@
 //applique le filtre noir et blanc sur l'onglet actif
 document.body.style.filter = "grayscale(100%)";
-console.log("blackandwhite");
+//console.log("blackandwhite");
+
+//filtre 2 : Hue-rotate
+document.body.style.filter = "hue-rotate(90deg)";
+
+//filtre 3 : Dropshadow
+document.body.style.filter = "invert(75%)";
