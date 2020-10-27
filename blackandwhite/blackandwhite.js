660280293c87946aa7af6294b8dbbbf521e2529e
Marine139 <marine.luciani@student.adatechschool.fr>
2020-10-27 15:21:31 +0100

    creation des trois boutons filtres

--- blackandwhite/blackandwhite.js
+++
@@ -1,9 +0,0 @@
-//applique le filtre noir et blanc sur l'onglet actif
-document.body.style.filter = "grayscale(100%)";
-//console.log("blackandwhite");
-
-//filtre 2 : Hue-rotate
-document.body.style.filter = "hue-rotate(90deg)";
-
-//filtre 3 : Dropshadow
-document.body.style.filter = "invert(75%)";
--- blackandwhite/popup.js
+++ blackandwhite/popup.js
@@ -1,11 +1,30 @@
 //déclaration des variables liées aux boutons activer et désactiver
 let buttonActivateGrayscale = document.getElementById('activateGrayscale');
+let buttonActivateHue = document.getElementById('activateHue');
+let buttonActivateDropshadow = document.getElementById('activateDropshadow');
 let buttonDesactivate = document.getElementById('desactivate');
 
 //fonction permettant d'exécuter le script blackandwhite.js en cliquant sur le bouton activer
+// Injection du code avec filtre à l'intérieur de tabs.executeScript
 buttonActivateGrayscale.addEventListener("click", function() {
       console.log('activateGrayscale');
-      browser.tabs.executeScript(null, { file: "blackandwhite.js" });
+      browser.tabs.executeScript({
+      code:'document.body.style.filter = "grayscale(100%)";'
+     });
+});
+
+buttonActivateHue.addEventListener("click", function() {
+      console.log('activateHue');
+      browser.tabs.executeScript({
+      code:'document.body.style.filter = "hue-rotate(90deg)";'
+     });
+});
+
+buttonActivateDropshadow.addEventListener("click", function() {
+      console.log('activateDropshadow');
+      browser.tabs.executeScript({
+      code:'document.body.style.filter = "invert(100%)";'
+     });
 });
 
 //fonction permettant de désactiver le filtre en rafraichissant la page en cliquant sur le bouton désactiver
@@ -13,5 +32,3 @@ buttonDesactivate.addEventListener("click", function() {
       console.log('desactivate');
       browser.tabs.reload();
 });
-
-//ajout variables et fonction pour filtre 2 et 3 (à faire)
