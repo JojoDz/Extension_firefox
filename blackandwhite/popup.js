b32841ba203ab68599237d5f7c64ed8738106dff
Francelin <67784025+Francel1n@users.noreply.github.com>
2020-10-27 17:15:58 +0100

    27/10

--- blackandwhite/popup.js
+++ blackandwhite/popup.js
@@ -2,33 +2,42 @@
 let buttonActivateGrayscale = document.getElementById('activateGrayscale');
 let buttonActivateHue = document.getElementById('activateHue');
 let buttonActivateDropshadow = document.getElementById('activateDropshadow');
-let buttonDesactivate = document.getElementById('desactivate');
-
 //fonction permettant d'exécuter le script blackandwhite.js en cliquant sur le bouton activer
 // Injection du code avec filtre à l'intérieur de tabs.executeScript
 buttonActivateGrayscale.addEventListener("click", function() {
       console.log('activateGrayscale');
-      browser.tabs.executeScript({
-      code:'document.body.style.filter = "grayscale(100%)";'
-     });
+      if (buttonActivateGrayscale.getAttribute("data-gris") == "true") {
+          buttonActivateGrayscale.innerHTML = "C'est désactivé";
+          browser.tabs.reload();
+          buttonActivateGrayscale.setAttribute("data-gris","false");
+    } else {
+          buttonActivateGrayscale.innerHTML = "C'est activé";
+          buttonActivateGrayscale.setAttribute("data-gris","true");
+          browser.tabs.executeScript({
+          code:'document.body.style.filter = "grayscale(100%)";'
+         });
+    }
 });
 
 buttonActivateHue.addEventListener("click", function() {
       console.log('activateHue');
+      if (buttonActivateHue.innerHTML == "off") {
+      buttonActivateHue.innerHTML = "on";
       browser.tabs.executeScript({
       code:'document.body.style.filter = "hue-rotate(90deg)";'
      });
+    } else {
+      buttonActivateHue.innerHTML = "off";
+      browser.tabs.executeScript({
+      code:'document.body.style.filter = "hue-rotate(0deg)";'
+     });
+    }
 });
 
 buttonActivateDropshadow.addEventListener("click", function() {
       console.log('activateDropshadow');
+      buttonActivateDropshadow.innerHTML = "Désactiver";
       browser.tabs.executeScript({
       code:'document.body.style.filter = "invert(100%)";'
      });
-});
-
-//fonction permettant de désactiver le filtre en rafraichissant la page en cliquant sur le bouton désactiver
-buttonDesactivate.addEventListener("click", function() {
-      console.log('desactivate');
-      browser.tabs.reload();
 });
